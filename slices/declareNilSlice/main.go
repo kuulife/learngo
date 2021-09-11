ac5fe168c4c3e486c66aef7645f477985ffc2de5
kuulife <kuu.life@gmail.com>
2021-09-11 12:22:40 +0600

    slices

--- slices/declareNilSlice/main.go
+++ slices/declareNilSlice/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	//declare a nil slices
@@ -17,5 +20,39 @@ func main() {
 	fmt.Printf("data: %T %d %t\n", data, len(data), data == nil)
 	fmt.Printf("ratious: %T %d %t\n", ratious, len(ratious), ratious == nil)
 	fmt.Printf("aliver: %T %d %t\n", aliver, len(aliver), aliver == nil)
+
+	// ---------------------------------------------------------
+	// EXERCISE: Assign slice literals
+	//
+	//   1. Assign the following data using slice literals to the slices that
+	//      you've declared in the first exercise.
+	//
+	//    1. The names of your three best friends (to the names slice)
+	fmt.Println(strings.Repeat("=", 20))
+	names = append(names, "Askat", "Chika", "Dastan")
+	fmt.Printf("names: %T %d %t\n", names, len(names), names == nil)
+	//
+	//    2. The distances to five different locations (to the distances slice)
+	distance = append(distance, 130.45, 5000.4, 180.0, 60, 45.67)
+	fmt.Printf("distance: %T %d %t\n", distance, len(distance), distance == nil)
+	//    3. Five bytes of data (to the data slice)
+	data = append(data, 1, 2, 3, 4, 5)
+	fmt.Printf("data: %T %d %t\n", data, len(data), data == nil)
+	//    4. Two currency exchange ratios (to the ratios slice)
+	ratious = append(ratious, 23.5, 3.7)
+	fmt.Printf("ratious: %T %d %t\n", ratious, len(ratious), ratious == nil)
+	//    5. Up/Down status of four different web servers (to the alives slice)
+	aliver = append(aliver, true, false, true, true)
+	fmt.Printf("alives: %T %d %t\n", aliver, len(aliver), aliver == nil)
+
+	//  3. Compare the length of the distances and the data slices; print a message
+	//     if they are equal (use an if statement).
+	var equal string
+
+	if len(distance) != len(data) {
+		equal = "not "
+	}
+
+	fmt.Printf("The length of the distances and the data slices are %sequal.", equal)
 
 }
