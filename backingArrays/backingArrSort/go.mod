6ecc807c03f57b6f1ae8e76cb7e8fe6f028b5aac
kuulife <kuu.life@gmail.com>
2021-09-12 16:39:11 +0600

    backing arr sort

---
+++ backingArrays/backingArrSort/go.mod
@@ -0,0 +1,3 @@
+module backingArrSort
+
+go 1.17
--- backingArrays/backingArrSort/main.go
+++ backingArrays/backingArrSort/main.go
@@ -1,1 +1,48 @@
-package backingArrSort
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+// ---------------------------------------------------------
+// EXERCISE: Sort the backing array
+//
+//  1. Sort only the middle 3 items.
+//
+//  2. All the slices should see your changes.
+//
+//
+// RESTRICTION
+//
+//  Do not sort manually. Sort by using the sort package.
+//
+//
+// EXPECTED OUTPUT
+//
+//  Original: [pacman mario tetris doom galaga frogger asteroids simcity metroid defender rayman tempest ultima]
+//
+//  Sorted  : [pacman mario tetris doom galaga asteroids frogger simcity metroid defender rayman tempest ultima]
+//
+//
+// HINT:
+//
+//   Middle items are         : [frogger asteroids simcity]
+//
+//   After sorting they become: [asteroids frogger simcity]
+//
+// ---------------------------------------------------------
+
+func main() {
+	items := []string{
+		"pacman", "mario", "tetris", "doom", "galaga", "frogger",
+		"asteroids", "simcity", "metroid", "defender", "rayman",
+		"tempest", "ultima",
+	}
+
+	fmt.Println("Original:", items)
+	// ADD YOUR CODE HERE
+	middle := items[len(items)/2-1 : len(items)/2+2]
+	sort.Strings(middle)
+	fmt.Println("Sorted  :", items)
+}
